b07d86d2ed0b3112763a82cf0d0ae536e1125d6e
herveleandre <47299858+herveleandre@users.noreply.github.com>
2023-01-25 20:02:13 -0500

    generate PDF invoice number

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("Invoices/*.xlsx")
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_number = filename.split("_")[0]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice Number.{invoice_number}")
+    pdf.output(f"{filename}.pdf")
+    print(df)
+print(filepaths)
+
